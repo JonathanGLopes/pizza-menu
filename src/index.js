1e951658de0789028998c5224cc891048ceabdb4
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-09-05 19:27:09 -0300

    passing and receiving props

--- src/index.js
+++ src/index.js
@@ -69,7 +69,18 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach and ricotta cheese"
+        price={12}
+        photoName="pizzas/spinaci.jpg"
+      />
+      <Pizza
+        name="Piza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={10}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
   );
 }
@@ -81,17 +92,20 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}We're currently open!
+      {new Date().toLocaleTimeString()} We're currently open!
     </footer>
   );
 }
 
-function Pizza() {
+function Pizza(props) {
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach and ricotta cheese</p>
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
