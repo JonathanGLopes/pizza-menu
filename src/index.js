40c5743876afda4e15e5c14c7bc70b3ddb38d94b
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-09-06 19:13:37 -0300

    removing variables

--- src/index.js
+++ src/index.js
@@ -86,10 +86,6 @@ function Menu() {
 }
 
 function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
-
   return (
     <footer className="footer">
       {new Date().toLocaleTimeString()} We're currently open!
